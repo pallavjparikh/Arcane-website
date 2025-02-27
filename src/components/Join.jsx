b1b81f0e3c3f12f7348f3e148b33a626a51ba848
Pallav Parikh <pallavjparikh@gmail.com>
2025-02-27 15:07:37 -0700

    test

--- src/components/Join.jsx
+++ src/components/Join.jsx
@@ -6,20 +6,21 @@ import "../styles/Join.css";
 function Join() {
     return (
         <div className='Block joinBlock' id='join'>
-            <center><p className='subHead' style={{ position: "relative", zIndex: 2 }}></p></center>
-            <center><p className="heading" style={{ position: "relative", zIndex: 2 }}>Get Started Today <br /><span style={{ color: "#ffffffb3" }}></span></p></center>
-
-                  
-            { <iframe className='joinBlock' 
-                src='https://forms.zohopublic.com/pallavthear1/form/ContactUS/formperma/6d1VdF1DMut7JWgRmVHic_kVY8G_BWj__KmElql28tc'
-                width="80%" 
-                align="center"
-                height="500px" 
-                style={{ border: "none", marginTop: "20px" }}
-                title="Join Form"
-            ></iframe> }
+            <div className='content'>
+                <div className='text'></div>
+                    <center><p className='subHead' style={{ position: "relative", zIndex: 2 }}></p></center>
+                    <center><p className="heading" style={{ position: "relative", zIndex: 2 }}>Get Started Today <br /><span style={{ color: "#ffffffb3" }}></span></p></center>
+                
+                <iframe className='joinBlock' 
+                    src='https://forms.zohopublic.com/pallavthear1/form/ContactUS/formperma/6d1VdF1DMut7JWgRmVHic_kVY8G_BWj__KmElql28tc'
+                    width="80%" 
+                    height="500px" 
+                    style={{ border: "none", marginTop: "20px" }}
+                    title="Join Form"
+                ></iframe>
+            </div>
         </div>
-    )
+    );
 }
 
-export default Join
+export default Join;
